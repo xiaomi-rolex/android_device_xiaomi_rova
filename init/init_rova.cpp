8c246009eb5acef19e93bdd913e6bc6c53c37cda
Michael Bestas <mikeioannina@cyanogenmod.org>
2017-09-14 15:55:35 -0300

    rova: Allow vendor init to override props
    Change-Id: I8a88292e8dbe4c6460c38bf4cde93097e7ed2516

--- init/init_rova.cpp
+++ init/init_rova.cpp
@@ -46,6 +46,17 @@ int property_set(const char *key, const char *value) {
 	    return __system_property_set(key, value);
 }
 
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
 void init_target_properties()
 {
     std::ifstream fin;
