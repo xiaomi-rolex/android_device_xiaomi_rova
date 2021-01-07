610366bf54371d6ffb3249117c5aafa7033b5e9a
Thagoo <lohitgowda56@gmail.com>
2021-01-07 15:36:40 +0000

    rova: extract-utils.sh: LINEAGE_ROOT --> ANDROID_ROOT

--- extract-files.sh
+++ extract-files.sh
@@ -23,11 +23,11 @@ export DEVICE_BRINGUP_YEAR=2017
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
 
 # Hax for cam configs
-CAMERA2_SENSOR_MODULES="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/libmmcamera2_sensor_modules.so
+CAMERA2_SENSOR_MODULES="$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/libmmcamera2_sensor_modules.so
 
 sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$CAMERA2_SENSOR_MODULES"
 
-DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"${VENDOR}"/"${DEVICE}"/proprietary
+DEVICE_BLOB_ROOT="$ANDROID_ROOT"/vendor/"${VENDOR}"/"${DEVICE}"/proprietary
 
 # Camera data
 for CAMERA_LIB in libmmcamera2_cpp_module.so libmmcamera2_dcrf.so libmmcamera2_iface_modules.so libmmcamera2_imglib_modules.so libmmcamera2_mct.so libmmcamera2_pproc_modules.so libmmcamera2_q3a_core.so libmmcamera2_sensor_modules.so libmmcamera2_stats_algorithm.so libmmcamera2_stats_modules.so libmmcamera_dbg.so libmmcamera_imglib.so libmmcamera_interface.so libmmcamera_pdaf.so libmmcamera_pdafcamif.so libmmcamera_tintless_algo.so libmmcamera_tintless_bg_pca_algo.so libmmcamera_tuning.so; do
